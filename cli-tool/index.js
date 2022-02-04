72ad2f39b6610c4921f6cf4a4a7f98790c95934b
ericbarriga <ericbarriga@MacBook-Pro.local>
2022-02-04 01:51:23 -0700

    added questions and wroked on orginization

--- cli-tool/index.js
+++ cli-tool/index.js
@@ -30,7 +30,7 @@ async function welcome() {
 
 }
 
-await welcome()
+
 
 async function askName() {
     const answer = await inquirer.prompt({
@@ -44,8 +44,8 @@ async function askName() {
     });
     playerName = answer.name;
 }
-await askName()
 
+//
 async function question1() {
     const answer = await inquirer.prompt({
         name: 'question1',
@@ -60,54 +60,55 @@ async function question1() {
     })
     return handleAnswer(answer.question1 == 'javacript is hard');
 }
-
-await question1()
-
-async function handleAnswer(isCorrect) {
-    const spinner = createSpinner('lets see if thats a good enough reason.....').start();
-    await sleep();
-
-    if (isCorrect) {
-        spinner.success({ text: `of thats a valid reason i guess ${playerName}` });
-    } else {
-        spinner.error({ text: ` thats not a good enough reason weirdo keep on living ${playerName}!!!!👆🏼👆🏼!!!` })
-        process.exit(1);
-    }
-
+//
+async function question2() {
+    const answer = await inquirer.prompt({
+        name: 'question1',
+        type: 'list',
+        message: 'why do you want to kill yourself????',
+        choices: [
+            'the housing market????',
+            'the ultra rich not paying taxes',
+            'the fact that mannies bitch ass makes it look so easy',
+            'boomers ... not you judo steve your cool as fuck 🥋 ',
+        ],
+    })
+    return handleAnswer(answer.question1 == 'the fact that mannies bitch ass makes it look so easy');
+}
+//
+async function question3() {
+    const answer = await inquirer.prompt({
+        name: 'question1',
+        type: 'list',
+        message: 'why do you want to kill yourself????',
+        choices: [
+            'the fact that they dont got money for teacher but they got money for war ... is that pac???',
+            'the fact that your stuck all day on somting and 😸rhys😸 and 🤓becca🤓 say its that word you missspelled',
+            'that the prison system is for profit and lobby the goverment to keep them full ',
+            'the fact that they blame imigrants for job loss but some big wig just sent all the jobs over seas and want to out source to more',
+        ],
+    })
+    return handleAnswer(answer.question1 == 'the fact that your stuck all day on somting and 😸rhys😸 and 🤓becca🤓 say its that word you missspelled');
 }
 
-await question2()
+// 
 
-async function handleAnswer(isCorrect) {
-    const spinner = createSpinner('lets see if thats a good enough reason.....').start();
-    await sleep();
-
-    if (isCorrect) {
-        spinner.success({ text: `of thats a valid reason i guess ${playerName}` });
-    } else {
-        spinner.error({ text: ` thats not a good enough reason weirdo keep on living ${playerName}!!!!👆🏼👆🏼!!!` })
-        process.exit(1);
-    }
-
+async function question4() {
+    const answer = await inquirer.prompt({
+        name: 'question1',
+        type: 'list',
+        message: 'why do you want to kill yourself????',
+        choices: [
+            'the fact that some guy with a brian injury gets it faster then i do ..<3 you matt please wear your helmet 🪖 🚵🏻',
+            'that steven Donziger was prosecuted by a private attorney/da because he won a case againt chevron in ecuador',
+            'that your still wating for the dip to go back up xrp to the moon',
+            'that big banks got a bail out and they will get another one this was typed on 2022 ',
+        ],
+    })
+    return handleAnswer(answer.question1 == 'the fact that some guy with a brian injury gets it faster then i do ..<3 you matt please wear your helmet 🪖 🚵🏻');
 }
 
-await question3()
-
-async function handleAnswer(isCorrect) {
-    const spinner = createSpinner('lets see if thats a good enough reason.....').start();
-    await sleep();
-
-    if (isCorrect) {
-        spinner.success({ text: `of thats a valid reason i guess ${playerName}` });
-    } else {
-        spinner.error({ text: ` thats not a good enough reason weirdo keep on living ${playerName}!!!!👆🏼👆🏼!!!` })
-        process.exit(1);
-    }
-
-}
-
-await question4()
-
+///
 async function handleAnswer(isCorrect) {
     const spinner = createSpinner('lets see if thats a good enough reason.....').start();
     await sleep();
@@ -125,11 +126,17 @@ async function handleAnswer(isCorrect) {
 function winner() {
     console.clear();
     const msg = `congrats, those ,reasons, are, good, enough , ${playerName} ! \n lets, go, get, the, rope `;
-
     figlet(msg, (err, data) => {
         console.log(gradient.pastel.multiline(data));
     })
 }
 
+
+await welcome()
+await askName()
+await question1()
+await question2()
+await question3()
+await question4()
 await winner()
 
